a3e6840e5cb9c8fae2fe80fbaaa47d0a00cdc833
olka01 <aleksandra92mielcarek@gmail.com>
2014-01-10 15:19:51 +0100

    prot

--- sciema/protocol.py
+++ sciema/protocol.py
@@ -21,13 +21,13 @@ class Protocol:
 			if order == 1:
 				self.server.location(message, from_user)
 			elif order == 2:
-				self.server.bomb(message, from_user)
+				self.server.bomb(message[0], from_user)
 			elif order == 3:
 				self.server.disconnect_user(from_user)
 			elif order == 4:
 				self.server.end_game(from_user)
 			elif order == 5:
-				self.server.solution_puzzle(message, from_user)
+				self.server.solution_puzzle(message[0], from_user)
 			elif order == 101:
 				self.server.create_game(from_user)
 			elif order == 102:
@@ -35,20 +35,22 @@ class Protocol:
 			elif order == 103:
 				self.server.start_game(from_user)
 			elif order == 104:
-				self.server.change_team(message, from_user)
+				self.server.change_team(message[0], from_user)
 			elif order == 105:
-				self.server.set_team_names(message, from_user)
+				self.server.set_team_names(message[0], message[1], from_user)
 			elif order == 201:
-				self.server.login(message, from_user)
+				self.server.login(message[0], message[1], from_user)
 			elif order == 202:
 				self.server.log_out(from_user)
 			elif order == 203:
-				self.server.registracion(message, from_user)
+				self.server.registracion(message[0], message[1], from_user)
 			elif order == 204:
-				self.server.change_pass(message, from_user)
+				self.server.change_pass(message[0], message[1], message[2], from_user)
 			elif order == 205:
-				self.server.new_pass_login(message, from_user)
+				self.server.new_pass_login(message[0], from_user)
 			else:
 				print("NIE MA TAKIEGO POLECENIA")
+		except IndexError:
+			from_user.get_messanger().answer_user(255)
 		except ServerError as e:
 			from_user.get_messanger().answer_user(e.code, e.msg)
